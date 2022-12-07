325cd215cc858decea9cfe9a6ad068bc38221435
Jesper Brown <jesper.brown@gmail.com>
2022-12-07 11:13:24 +0100

    day1 part2 done

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,11 @@
+use std::env;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
     let mut calories_scoreboard = Vec::new();
-    let input = include_str!("example_input.txt");
-    //let input = include_str!("input.txt");
+    //let input = include_str!("example_input.txt");
+    let input = include_str!("input.txt");
 
     let mut calories: u32 = 0;
 
@@ -15,5 +19,13 @@ fn main() {
         
     }
     calories_scoreboard.sort();
-    println!("{:?}", calories_scoreboard[calories_scoreboard.len() - 1])
+
+    if args.len() > 1 {
+        // part2 switch by just having input args
+        let top_three_sum: u32 = calories_scoreboard.iter().rev().take(3).sum();
+        println!("{}", top_three_sum);
+    } else {
+        println!("{:?}", calories_scoreboard[calories_scoreboard.len() - 1])
+    }
+    
 }
