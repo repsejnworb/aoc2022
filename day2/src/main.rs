4b17151ec640c7a344c7583b3bde111eb2dddf62
Jesper Brown <jesper.brown@gmail.com>
2022-12-07 15:27:45 +0100

    day2 part2 done (with modifications to day1)

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -42,15 +42,27 @@ let args: Vec<String> = env::args().collect();
 
 use std::env;
 
-fn translate_shape(letter: &str) -> i32 {
-    match letter {
-        "A" => 1,
-        "B" => 2,
-        "C" => 3,
-        "X" => 1,
-        "Y" => 2,
-        "Z" => 3,
-        &_ => 0
+fn translate_code(code: &str, part: i32) -> i32 {
+    if part == 1 {
+        match code {
+            "A" => 1, // rock
+            "B" => 2, // paper
+            "C" => 3, // scissor
+            "X" => 1, // rock
+            "Y" => 2, // paper
+            "Z" => 3, // scissor
+            &_ => 0 // fallback
+        }
+    } else {
+        match code {
+            "A" => 1,
+            "B" => 2,
+            "C" => 3,
+            "X" => 0,
+            "Y" => 3,
+            "Z" => 6,
+            &_ => 0
+        }
     }
 }
 
@@ -65,12 +77,25 @@ fn get_score(outcome: i32) -> i32 {
     }
 }
 
+fn clamp_wrap(value: i32) -> i32 {
+    // Clamps and wraps value between min and max, if greater than max returns
+    // min and if lesser than min returns max
+    let min = 1;
+    let max = 3;
+    if value > max {
+        return min;
+    } else if value < min {
+        return max;
+    } else {
+        return value;
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut _part = 1;
+    let mut part = 1;
     if args.len() > 1 {
-        _part = 2;
+        part = 2;
     }
 
     //let input: &str = include_str!("example_input.txt");
@@ -78,11 +103,23 @@ fn main() {
     let mut total_score: i32 = 0;
 
     for round in input.lines() {
-        let mut round_shapes = round.split_whitespace();
-        let round = (round_shapes.next().unwrap(), round_shapes.next().unwrap());
-        let result: i32 = translate_shape(round.0) - translate_shape(round.1);
-        total_score += translate_shape(round.1);
-        total_score += get_score(result);
+        let mut codes = round.split_whitespace();
+        if part == 1 {
+            let opponent_shape = translate_code(codes.next().unwrap(), part);
+            let my_shape = translate_code(codes.next().unwrap(), part);
+            let result: i32 = opponent_shape - my_shape;
+            total_score += my_shape + get_score(result);
+        } else {
+            let shape = translate_code(codes.next().unwrap(), part);
+            let outcome = translate_code(codes.next().unwrap(), part);
+            total_score += match outcome {
+                0 => clamp_wrap(shape - 1) + 0,
+                3 => clamp_wrap(shape) + 3,
+                6 => clamp_wrap(shape + 1) + 6,
+                i32::MIN..=-3_i32 | 3_i32..=i32::MAX => todo!(),
+                -2_i32..=-1_i32 | 1_i32..=2_i32 => todo!()
+            };
+        }
     }
 
     println!("Total score: {}", total_score);  
