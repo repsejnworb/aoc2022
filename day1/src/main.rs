27278707a1fe600d96a0cc861bae2c6a8902e67c
Jesper Brown <jesper.brown@gmail.com>
2022-12-07 11:05:13 +0100

    Modified day1_part1 to use a Vec

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,24 +1,19 @@
-use std::{collections::HashMap};
-
 fn main() {
-    let mut gnomes = HashMap::new();
-    //let input = include_str!("example_input.txt");
-    let input = include_str!("input.txt");
+    let mut calories_scoreboard = Vec::new();
+    let input = include_str!("example_input.txt");
+    //let input = include_str!("input.txt");
 
-
-    let mut gnome_id: u32 = 0;
-    let mut gnome_calories: u32 = 0;
+    let mut calories: u32 = 0;
 
     for line in input.lines() {
         if line.is_empty() {
-            gnomes.insert(format!("gnome{}", gnome_id), gnome_calories);
-            gnome_id += 1;
-            gnome_calories = 0;
+            calories_scoreboard.push(calories);
+            calories = 0;
         } else {
-            gnome_calories += line.parse::<u32>().unwrap();
+            calories += line.parse::<u32>().unwrap();
         }
         
     }
-    let gnome_with_most_calories = gnomes.iter().max_by_key(|entry | entry.1).unwrap();
-    println!("{}: {}", gnome_with_most_calories.0, gnome_with_most_calories.1);
+    calories_scoreboard.sort();
+    println!("{:?}", calories_scoreboard[calories_scoreboard.len() - 1])
 }
